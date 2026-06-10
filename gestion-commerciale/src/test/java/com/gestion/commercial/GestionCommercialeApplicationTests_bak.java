6e2c3b622c61ce6128927ca5ca75f402e7d4ee41
Mammane <mammanebi@gmail.com>
2026-06-10 16:11:08 +0200

    premier commit

---
+++ gestion-commerciale/src/test/java/com/gestion/commercial/GestionCommercialeApplicationTests_bak.java
@@ -0,0 +1,13 @@
+package com.gestion.commercial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCommercialeApplicationTests_bak {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
